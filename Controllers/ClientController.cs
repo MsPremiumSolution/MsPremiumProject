8d12e4e9cbbf46451c9a7b01234fd680aa9e7ccc
MsPremiumSolution <mspremiumsolution@gmail.com>
2025-06-18 10:40:44 +0100

    Add View Client

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSPremiumProject.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ClientPainel()
+        {
+            return View("~/Views/Client/ClientPainel.cshtml");
+        }
+    }
+}
