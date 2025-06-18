4f60098980fcd12676aed083a9129d62f4905420
MsPremiumSolution <mspremiumsolution@gmail.com>
2025-06-18 13:39:35 +0100

    viewsLocalidade

---
+++ Controllers/LocalityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSPremiumProject.Controllers
+{
+    public class LocalityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult InserirLocalidade()
+        {
+            return View("~/Views/Locality/CreateLocality.cshtml");
+        }
+    }
+}
