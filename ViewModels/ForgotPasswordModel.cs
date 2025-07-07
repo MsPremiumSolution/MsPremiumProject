14f988f457fc05decfd01a978805de3b07c29b48
MsPremiumSolution <mspremiumsolution@gmail.com>
2025-07-07 16:55:20 +0100

    teste

--- ViewModels/ForgotPasswordModel.cs
+++ ViewModels/ForgotPasswordModel.cs
@@ -7,7 +7,7 @@ namespace MSPremiumProject.ViewModels // Certifique-se que este namespace corres
     public class ForgotPasswordViewModel
     {
         [Required(ErrorMessage = "O campo Login é obrigatório.")]
-        [Display(Name = "Login")] // Como o campo será exibido em labels, se usar asp-label-for
+        [Display(Name = "Email")] // Como o campo será exibido em labels, se usar asp-label-for
         [StringLength(100, ErrorMessage = "O login deve ter no máximo {1} caracteres.")] // Ajuste o tamanho máximo se necessário
         public string Login { get; set; }
     }
--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -6,7 +6,7 @@ namespace MSPremiumProject.ViewModels
     public class LoginViewModel
     {
         [Required(ErrorMessage = "O email ou login é obrigatório.")]
-        [Display(Name = "Email ou Login")]
+        [Display(Name = "Email")]
         public string Login { get; set; }
 
         [Required(ErrorMessage = "A password é obrigatória.")]
